ed941ec3bcba00a8919cd9fff725cb3fe0d13b10
Ste517 <ste517@users.noreply.github.com>
2022-06-01 15:21:40 +0200

    testcalcolatore6

--- calcolatore.js
+++ calcolatore.js
@@ -1,14 +1,16 @@
 function calcola() {
-    a = 0.7;
-    u = Number(document.getElementById("u").value);
-    c = Number(document.getElementById("c").value);
-    i = Number(document.getElementById("i").value);
-    s = Number(document.getElementById("s").value);
-    f = Number(document.getElementById("f").value);
+    
+    // get the data
+    var a = 0.7;
+    var u = Number(document.getElementById("u").value);
+    var c = Number(document.getElementById("c").value);
+    var i = Number(document.getElementById("i").value);
+    var s = Number(document.getElementById("s").value);
+    var f = Number(document.getElementById("f").value);
 
-    result = (a*(10-s)*(c+i+u))/(2*(1-Math.sin(f/10)));
+    var result = (a*(10-s)*(c+i+u))/(2*(1-Math.sin(f/10)));
 
-    resultstr = toString(result);
+    var resultstr = toString(result);
 
     document.getElementById("result").innerHTML = resultstr;
 }
